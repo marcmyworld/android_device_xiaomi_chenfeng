673cc63c1a9706932874f6d706457fbdc9cec77a
marcmyworld <moviesminister@gmail.com>
2026-09-10 01:11:44 +0530

    chenfeng: Update vendorsetup.sh to new repositories

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,13 +2,13 @@
 # This script for auto cloning some stuff before building
 
 # Clone kernel tree
-git clone https://github.com/Little-Chenfeng-ID/device_xiaomi_chenfeng-kernel.git -b lineage-23.2 device/xiaomi/chenfeng-kernel
+git clone https://gitlab.com/xiaomi-chenfeng/android_device_xiaomi_chenfeng-kernel.git -b lineage-23.2 device/xiaomi/chenfeng-kernel
 
 # Clone hardware xiaomi
-git clone https://github.com/Little-Chenfeng-ID/hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
+git clone https://gitlab.com/xiaomi-chenfeng/android_hardware_xiaomi-chenfeng.git -b lineage-23.2 hardware/xiaomi
 
 # Clone vendor device chenfeng
-git clone https://codeberg.org/smgreborn/vendor_xiaomi_chenfeng.git -b lineage-23.2 vendor/xiaomi/chenfeng
+git clone https://gitlab.com/xiaomi-chenfeng/vendor_xiaomi_chenfeng.git -b lineage-23.2 vendor/xiaomi/chenfeng
 
 # Finish clone all stuff
 # Happy Build and Brick
