781a26a76b88880042030c1e06bd04fcfc724dc5
marcmyworld <moviesminister@gmail.com>
2026-09-21 01:55:27 +0530

    fixup! chenfeng: Update system WFD stack from marvel_g A171WEH.20
    Change-Id: Ie688a0d0bc4b83443cd9a791a06c97ad13579406

--- extract-files.py
+++ extract-files.py
@@ -55,7 +55,10 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
-
+    'system/framework/WfdCommon.jar': blob_fixup()
+        .apktool_patch('blob-patches/WfdCommon.patch'),
+    'system_ext/etc/init/qspa_system.rc': blob_fixup()
+        .regex_replace(r'\$\{ro\.boot\.vendor\.qspa:-default\}', 'default'),
     'system_ext/etc/vintf/manifest/vendor.qti.qesdsys.service.xml': blob_fixup()
         .regex_replace(r'(?s)^.*?(?=<manifest)', ''),
     'system_ext/lib64/libwfdservice.so': blob_fixup()
