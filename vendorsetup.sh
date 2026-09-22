d8862077bec48526ebfd110b9089573722b1e517
marcmyworld <moviesminister@gmail.com>
2026-09-22 17:31:05 +0530

    chenfeng: Add miuicamera and dolby trees in vendorsetup.sh and bump branch to lineage-24.0

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,20 +1,39 @@
-# Welcome to SMGReborn Teams
+# Welcome to xiaomi-chenfeng aosp trees
 # This script for auto cloning some stuff before building
 
 # Clone kernel tree
 if [ ! -d "device/xiaomi/chenfeng-kernel" ]; then
-    git clone https://gitlab.com/xiaomi-chenfeng/android_device_xiaomi_chenfeng-kernel.git -b lineage-23.2 device/xiaomi/chenfeng-kernel
+    git clone https://gitlab.com/xiaomi-chenfeng/android_device_xiaomi_chenfeng-kernel.git -b lineage-24.0 device/xiaomi/chenfeng-kernel
 fi
 
 # Clone hardware xiaomi
 if [ ! -d "hardware/xiaomi" ]; then
-    git clone https://gitlab.com/xiaomi-chenfeng/android_hardware_xiaomi-chenfeng.git -b lineage-23.2 hardware/xiaomi
+    git clone https://gitlab.com/xiaomi-chenfeng/android_hardware_xiaomi-chenfeng.git -b lineage-24.0 hardware/xiaomi
 fi
+
+# Clone miuicamera
+if [ ! -d "device/xiaomi/chenfeng-miuicamera" ]; then
+    git clone https://gitlab.com/xiaomi-chenfeng/android_device_xiaomi_chenfeng-miuicamera.git -b lineage-24.0 device/xiaomi/chenfeng-miuicamera
+fi
+
+
+## BELOW ARE PROPRIETARY BLOBS
 
 # Clone vendor device chenfeng
 if [ ! -d "vendor/xiaomi/chenfeng" ]; then
-    git clone https://gitlab.com/xiaomi-chenfeng/vendor_xiaomi_chenfeng.git -b lineage-23.2 vendor/xiaomi/chenfeng
+    git clone https://gitlab.com/xiaomi-chenfeng/vendor_xiaomi_chenfeng.git -b lineage-24.0 vendor/xiaomi/chenfeng
 fi
+
+# Clone vendor miuicamera
+if [ ! -d "vendor/xiaomi/chenfeng-miuicamera" ]; then
+    git clone https://gitlab.com/xiaomi-chenfeng/vendor_xiaomi_chenfeng-miuicamera.git -b lineage-24.0 vendor/xiaomi/chenfeng-miuicamera
+fi
+
+# Clone custom dolby
+if [ ! -d "hardware/dolby" ]; then
+    git clone https://gitlab.com/xiaomi-chenfeng/hardware_dolby.git -b 17 hardware/dolby
+fi
+
 
 # Apply required source tree patches for chenfeng
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
